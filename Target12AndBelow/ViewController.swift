b1cc9b65d6b04426dd48adf5cd0e34812dac1993
eldin <eldinsmakic@hotmail.fr>
2020-04-13 18:58:47 +0200

    [Feat] : add constraint to iPhone format

---
+++ Target12AndBelow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Target12AndBelow
+//
+//  Created by eldin smakic on 12/04/2020.
+//  Copyright © 2020 eldin smakic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
